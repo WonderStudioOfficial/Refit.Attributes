d18aebef62e78c97bd339fa9bb6c4c4dc044161c
Константин Медведев <nikolaevkostya2004@gmail.com>
2025-02-19 18:14:30 +0300

    Add project files.

---
+++ src/Wonder.Refit.Attributes/WonderHttpMethodAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+
+namespace Wonder.Refit.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public abstract class WonderHttpMethodAttribute(string path, HttpMethod httpMethod) : HttpMethodAttribute(path)
+{
+    public override HttpMethod Method { get; } = httpMethod;
+
+    public override string Path
+    {
+        get => $"/{base.Path.Trim(' ', '/')}";
+        protected set => base.Path = value;
+    }
+}
---
+++ src/Wonder.Refit.Attributes/WonderHttpMethodAttributes.cs
@@ -0,0 +1,7 @@
+﻿namespace Wonder.Refit.Attributes;
+
+public class WonderDeleteAttribute(string path) : WonderHttpMethodAttribute(path, HttpMethod.Delete);
+public class WonderGetAttribute(string path) : WonderHttpMethodAttribute(path, HttpMethod.Get);
+public class WonderPatchAttribute(string path) : WonderHttpMethodAttribute(path, HttpMethod.Patch);
+public class WonderPostAttribute(string path) : WonderHttpMethodAttribute(path, HttpMethod.Post);
+public class WonderPutAttribute(string path) : WonderHttpMethodAttribute(path, HttpMethod.Put);
